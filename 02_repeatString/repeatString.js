af067029d86a2bf999e406b32ccd1846bcc8f92d
Drelka <47503020+Drelka@users.noreply.github.com>
2024-01-10 13:00:41 +0000

    Add Jest 02string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 0;
+    result = "";
 
+    if (num > 0) {
+        do{ result += string;
+            i++;
+        } while(i < num);
+    } else if (num < 0) {
+        console.log("Something went wrong. MISSION ABORTED.");
+        return "ERROR";  
+    } else {}
+    
+    return result;
 };
 
 // Do not edit below this line
