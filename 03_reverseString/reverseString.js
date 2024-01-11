23f6e4a37e82f1f5f42dbf7082980f5372f249db
Drelka <47503020+Drelka@users.noreply.github.com>
2024-01-11 12:55:22 +0000

    Add 03 reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let toArray = word.split("");
+    let backToFront = toArray.reverse();
+    let toString = backToFront.join("");
+    return toString;
 };
 
 // Do not edit below this line
