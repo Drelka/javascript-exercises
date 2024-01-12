b0b1a20947433fb8cd1cb59f9c6721cb29562660
Drelka <47503020+Drelka@users.noreply.github.com>
2024-01-12 03:01:35 +0000

    Add exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+// get two inputs:
+    // #1 an array
+    // #2 some other arguments
+// if 2# is in 1#
+    // remove 2# arguments from the 1# array
+        // else -ignore
 
+const removeFromArray = function(arr, ...arg) {
+    const anArray = [];
+    arr.forEach((element) => {
+        if (!arg.includes(element)) {
+            anArray.push(element);
+        }
+    });
+    return anArray;
 };
 
 // Do not edit below this line
