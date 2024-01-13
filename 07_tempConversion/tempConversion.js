781bf97f6d73f39c8d9629e5cc8557e4a070dcb2
Drelka <47503020+Drelka@users.noreply.github.com>
2024-01-13 22:01:55 +0000

    Add exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFar) {
+  return parseFloat(((tempInFar  - 32) * 5/9).toFixed(1)); 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+  return parseFloat((tempInCel * 9/5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
