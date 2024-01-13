851df37a9a4033ccfd565174745301544d79ba29
Drelka <47503020+Drelka@users.noreply.github.com>
2024-01-13 20:30:03 +0000

    Add exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(low, high) {
+    let sum = 0;
+    let isNumber = function (val){
+        return typeof val === "number";
+    }
+    if (low < 0 || high < 0 || !isNumber(high) || !isNumber(low)) return "ERROR";
+    
+    if (low > high){
+        const x = low;
+        low = high;
+        high = x;
+    }
+    for (i = low; i <= high; i++){
+        sum += i;
+    }    
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+/*
+ take (x to y) (ex 1 to 4)
+ create sumXtoY = 0
+ for (i = x ; i <= y ; i++)
+  {sumXtoY += i}
+*/
+ 
+ 
