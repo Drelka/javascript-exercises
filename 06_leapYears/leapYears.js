fcd03fe6c3f5a14a2463efcee278d2535be568b6
Drelka <47503020+Drelka@users.noreply.github.com>
2024-01-13 21:33:49 +0000

    Add exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0 && ( year % 400 == 0 || !(year % 100 == 0) )){
+            return true;
+        } else {
+            return false;
+        }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/*
+use *year* agrgument in a function
+check if it is a leap year:
+    divisible by 4
+    not divisible by 100, unless divisible by 400
+return boolean value (true or false)
+*/
