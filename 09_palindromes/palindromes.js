9910306c0468c74c2d7f7fa87e8ab077ced58419
Drelka <47503020+Drelka@users.noreply.github.com>
2024-03-03 17:10:03 +0000

    Exercises 09 and 10 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+//CHECK FOR STRING LENGTH (STRING LENGTH -1 === MAX INDEX)
+    //CREATE LOOP:
+        //1st SOLUTION (charAt() METHOD)
+            //(CHECK IF CHAR AT INDEX 1 === CHAR AT INDEX -1)
+            //         (CHAR AT INDEX 2 === CHAR AT INDEX -2 AND SO ON...)
+                // ONE LOOP INCREMENTING FROM "0", ANOTHER DECREMENTING FROM "STRING LENGTH - 1"
+        //2nd SOLUTION
+            //CREATE NEW STRING, REVERSING THE OLD ONE
+            //COMPARE STRINGS
 
+
+
+const palindromes = function (string) {
+    const string2 = string.toLowerCase().replace(/[\.,?! ]/g, "");
+    return string2 === string2.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+// FUNCTION RETURNING FIBONACCI SEQUENCE
+// (1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ... AND SO ON)
 
+// func(1) = 1
+// func(2) = 1
+// func(3) = 2 (1+1) (index[1] + index[2])
+// func(4) = 3 (1+2) (index[2] + index[3])
+// func(5) = 5 (2+3) (index[3] + index[4])
+// func(6) = 8 (3+5) (index[4] + index[5])
+// func(7) = 13 ...
+
+// so func(x) = [x-2] + [x-1]
+
+
+
+
+
+const fibonacci = function(count) {
+    if(count < 0) return "OOPS";
+    const fib = [0, 1];
+    let current = 2;
+    for(let i = current; i <= count; i++){
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return(fib[count]);
 };
 
 // Do not edit below this line
